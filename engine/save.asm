dfe59cff8b589bd09cca2c9a69c73513b4927b81
Adelaide Walter <adelaidekwalter@gmail.com>
2017-05-12 12:43:14 -0400

    Add constant CHUNKS_PER_BOX.

--- engine/save.asm
+++ engine/save.asm
@@ -881,13 +881,92 @@ endr
 ; 150f9
 
 SaveBoxAddress:
-	save_box_address_chunks 4
+	push hl
+	push af
+	push de
+i = 0
+REPT CHUNKS_PER_BOX
+	ld a, BANK(sBox)
+	call GetSRAMBank
+	ld hl, sBox + (wMiscEnd - wMisc) * i
+	ld de, wMisc
+IF i + 1 < CHUNKS_PER_BOX
+	ld bc, (wMiscEnd - wMisc)
+ELSE
+	ld bc, sBoxEnd - (sBox + (wMiscEnd - wMisc) * i)
+ENDC
+	call CopyBytes
+	call CloseSRAM
+	pop de
+	pop af
+IF i > 0
+	ld hl, (wMiscEnd - wMisc)
+	add hl, de
+	ld e, l
+	ld d, h
+ENDC
+IF i + 1 < CHUNKS_PER_BOX
+	push af
+	push de
+ENDC
+	call GetSRAMBank
+	ld hl, wMisc
+IF i + 1 < CHUNKS_PER_BOX
+	ld bc, (wMiscEnd - wMisc)
+ELSE
+	ld bc, sBoxEnd - (sBox + (wMiscEnd - wMisc) * i)
+ENDC
+	call CopyBytes
+	call CloseSRAM
+i = i + 1
+ENDR
+	pop hl
 	ret
+
 
 LoadBoxAddress:
-	load_box_address_chunks 4
+	push hl
+	ld l, e
+	ld h, d
+i = 0
+REPT CHUNKS_PER_BOX
+IF i > 0
+	ld de, (wMiscEnd - wMisc)
+	add hl, de
+ENDC
+IF i + 1 < CHUNKS_PER_BOX
+	push af
+	push hl
+ENDC
+	call GetSRAMBank
+	ld de, wMisc
+IF i + 1 < CHUNKS_PER_BOX
+	ld bc, (wMiscEnd - wMisc)
+ELSE
+	ld bc, sBoxEnd - (sBox + (wMiscEnd - wMisc) * i)
+ENDC
+	call CopyBytes
+	call CloseSRAM
+	ld a, BANK(sBox)
+	call GetSRAMBank
+	ld hl, wMisc
+	ld de, sBox + (wMiscEnd - wMisc) * i
+IF i + 1 < CHUNKS_PER_BOX
+	ld bc, (wMiscEnd - wMisc)
+ELSE
+	ld bc, sBoxEnd - (sBox + (wMiscEnd - wMisc) * i)
+ENDC
+	call CopyBytes
+	call CloseSRAM
+	pop hl
+IF i + 1 < CHUNKS_PER_BOX
+	pop af
+ENDC
+i = i + 1
+ENDR
 	ret
 
+	
 EraseBoxes: ; 151fb
 	ld hl, BoxAddresses
 	ld c, NUM_BOXES
